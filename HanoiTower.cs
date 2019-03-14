dcf017fc3f0d5964dd6f1d22fdb353310881e1e6
Rabbidon <edwinfennell1@gmail.com>
2019-03-14 17:52:37 +0000

    Added Tower of Hanoi in C#

---
+++ HanoiTower.cs
@@ -0,0 +1,27 @@
+﻿//A quick implementation of a tower of Hanoi solver - made with the intention of gaining familiarity with C#
+using System;
+using System.Collections.Generic;
+
+namespace Algorithms
+{
+    public class HanoiTower{
+        public static void Main(string[] args)
+        {
+            solve(2,"LEFT","RIGHT","MIDDLE");
+        }
+        public static void solve(int n, string origin, string destination, string other)
+        {
+            if (n==1)
+            {
+                Console.WriteLine(origin+"-->"+destination);
+            }
+            else
+            {
+                solve(n-1,origin,other,destination);
+                Console.WriteLine(origin+"-->"+destination);
+                solve(n-1,other,destination,origin);
+            }
+
+        }
+    }
+}
